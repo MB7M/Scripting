b3d799d91cb20ed4788db4cf7150f642a55a60e6
MB7M <mohamed.bouzarouata@gmail.com>
2024-07-19 00:14:51 +0200

    Update maj_phpext.sh

--- maj_phpext.sh
+++ maj_phpext.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Mettre à jour les paquets
 apt update
-apt install -y php8.0-intl php8.0-bz2 php8.0-ldap php8.0-openssl php8.0-imagick php8.0-gd php8.0-mysql php8.0-xml php8.0-mbstring php8.0-curl php8.0-zip libapache2-mod-php8.0
+
+# Installer les extensions PHP manquantes
+apt install -y php8.0-intl php8.0-bz2 php-ldap php-imagick php8.0-gd php8.0-mysql php8.0-xml php8.0-mbstring php8.0-curl php8.0-zip libapache2-mod-php8.0
+
+# Redémarrer le serveur Apache
 systemctl restart apache2
+
+echo "Les extensions PHP manquantes ont été installées et Apache a été redémarré."
+
